23a524338b52275b29378ba769428ab5e33b95bc
hq <602997434@qq.com>
2021-11-14 19:24:08 -0800

    应用

--- application.go
+++ application.go
@@ -12,4 +12,37 @@
 
 	生命周期
 		
+					mqant Run
+						|						——————>  mqant stop	
+						|						|			 |
+					初始化配置					|			 |
+						|						|		  停止模块
+						|						|			 |
+			app.configurationLoaded()			|			 |
+						|						|	等待指定时间后如模块还
+						|						|	未停止成功则强杀进程
+		 mods.OnAppConfigurationLoaded()		|			 |
+						|						|			 |
+						|						|		 mqant end
+					初始化模块					|
+						|						|
+						|						|
+				  app.startup()			 ———————>
+
+	配置解析完成
+		_ = app.OnConfigurationLoaded(func(app module.App) {
+
+		})
+
+	应用启动完成
+		包括模块启动完成
+		app.OnStartup(func (app module.App) {
+
+		})
+
+	设置强杀时间
+		当出现模块卡死等无法退出进情况下，超过设置时间会强杀
+		app := mqant.CreateApp(
+			module.KillWaitTTL(1 * time.Minute)
+		)
 */	
