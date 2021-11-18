056277fe22cbe6b9f07f29a32a14e2cbb99d2aa2
hq <602997434@qq.com>
2021-11-18 01:38:24 -0800

    第一个handler

--- first_handler.go
+++ first_handler.go
@@ -84,4 +84,197 @@
 		}
 
 	编写第一个handler
+		package helloworld
+
+		import (
+			"fmt"
+			"github.com/liangdas/mqant/conf"
+			"github.com/liangdas/mqant/log"
+			"github.com/liangdas/mqant/module"
+			"github.com/liangdas/mqant/module/base"
+		)
+
+		var Module = func() module.Module {
+			this := new(HelloWorld)
+			return this
+		}
+
+		type HelloWorld struct {
+			basemodule.BaseModule
+		}
+
+		func (self *HelloWorld) GetType() string {
+			// 很关键，需要与配置文件中的Module配置对应
+			return "helloworld"
+		}
+
+		func (self *HelloWorld) Version() string {
+			// 可以在监控时了解代码版本
+			return "1.0.0"
+		}
+
+		func (self *HelloWorld) OnInit(app module.App, settings *conf.ModuleSettings) {
+			self.BaseModule.OnInit(self, app, settings)
+			self.GetServer().RegisterGO("/say/hi", self.say) // handler
+			log.Info("%v模块初始化完成...", self.GetType())
+		}
+
+		func (self *HelloWorld) OnDestory() {
+			// 一定别忘了继承
+			// self.BaseModule.OnDestory()
+			log.Info("%v模块已回收...", self.GetType())
+		}
+
+		func (self *HelloWorld) say(name string) (r string, err error) {
+			return fmt.Sprintf("hi %v", name), nil
+		}
+
+		新增handler函数
+			name 为传入值
+			r 为函数正常处理err情况下的返回内容
+			err 为函数异常处理情况下的返回内容
+			func say(name string) (r string, err error)
+
+		将handler注册到模块中
+			/say/hi 为访问地址
+			self.GetServer().RegistryGO("/say/hi", self.say)
+
+	创建一个web模块
+		在helloworld模块中实现了第一个handler，但是没有地方在使用它，因此我们编写一个web模块尝试通过http
+		能调用这个handler
+			package web
+
+			import (
+				"context"
+				"github.com/liangdas/mqant/conf"
+				"github.com/liangdas/mqant/log"
+				"github.com/liangdas/mqant/module"
+				"github.com/liangdas/mqant/module/base"
+				"github.com/liangdas/mqant/rpc"
+				"io"
+				"net/http"
+			)
+
+			var Module = func() module.Module {
+				this := new(Web)
+				return this
+			}
+
+			type Web struct {
+				basemodule.BaseModule
+			}
+
+			func (self *Web) GetType() string {
+				// 很关键，需要与配置文件中的Module配置对应
+				return "Web"
+			}
+
+			func (self *Web) Version() string {
+				// 可以在监控时了解代码版本
+				return "1.0.0"
+			}
+
+			func (self *Web) OnInit(app module.App, settings *conf.ModuleSettings) {
+				self.BaseModule.OnInit(self, app, settings)
+			}
+
+			func (self *Web)startHttpServer() *http.Server {
+				srv := &http.Server{Addr: ":8080"}
+				http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+					_ = r.ParseFrom()
+					rstr, err := mqrpc.String(
+						self.Call(
+							context.Background(),
+							"helloworld",
+							"/say/hi",
+							mqrpc.Param(r.Form.Get("name")),
+						),
+					)
+					log.Info("RpcCall %v, err %v", rstr, err)
+					_, _ = io.WriteString(w, rstr)
+				})
+
+				go func() {
+					if err := srv.ListenAndServe(); err != nil {
+						// cannot panic, because this probably is an itentional close
+						log.Info("Httpserver: ListenAndServe() error: %s", err)
+					}
+				}()
+				// returning reference so caller can call Shutdown()
+				return srv
+			}
+
+			func (self *Web) Run(closeSig chan bool) {
+				log.Info("web: starting HTTP server : 8080")
+				srv := self.startHttpServer()
+				<-closeSig
+				log.Info("web: stopping HTTP server")
+				// now close the server gracefully ("shutdown")
+				// timeout could be given instead of nil as a https://golang.org/pkg/context/
+				if err := srv.Shutdown(nil); err != nil {
+					panic(err) // failure/timeout shutting down the server gracefully
+				}
+				log.Info("web: done. exiting")
+			}
+
+			func (self *Web) onDestory() {
+				// 一定别忘了继承
+				self.BaseModule.OnDestory()
+			}
+
+	特性
+		1.web模块对外监听8080端口
+			http://127.0.0.1:8080/say?name=mqant
+		
+		2.web模块收到请求后，通过RPC调用helloworld模块提供的handler，并将结果返回客户端
+			http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+				_ = r.ParseForm()
+				rstr, err = mqrpc.String(
+					self.Call(
+						conext.Background(),
+						"helloworld",
+						"/say/hi",
+						mqrpc.Param(r.Form.Get("name")),
+					),
+				)
+				log.Info("RpcCall %v, err %v", rstr, err)
+				_, _ = io.WriteString(w, rstr)
+			})
+
+	注意事项
+		1.web模块需要加入main.go函数入口中
+			err = app.Run( //模块都需要加到入口列表中传入框架
+				helloworld.Module(),
+				web.Module(),
+			)
+
+		2.web模块需要在mqant配置文件中添加配置
+			"Module":{
+				"helloworld":[
+					{
+						"Id":"helloworld",
+						"ProcessID":development
+					}
+				],
+				"Web":[
+					"Id":"Web001",
+					"ProcessID":"development"
+				]
+			},
+
+	运行
+		Server configuration path : //work/go/mqant-helloworld/bin/conf/server.json
+		2020-05-03T19:59:19.643351+08:00 [-] [-] [development] [I] [app.go:203] mqant  starting up
+		2020-05-03T19:59:19.643603+08:00 [-] [-] [development] [I] [ModuleManager.go:50] This service ModuleGroup(ProcessID) is [development]
+		2020-05-03T19:59:19.643762+08:00 [-] [-] [development] [I] [rpc_server.go:142] Registering node: HelloWorld@50d3be3d45da93b6
+		2020-05-03T19:59:19.64951+08:00 [-] [-] [development] [I] [module.go:34] HelloWorld模块初始化完成...
+		2020-05-03T19:59:19.649629+08:00 [-] [-] [development] [I] [rpc_server.go:142] Registering node: Web@6b3350636a9387de
+		2020-05-03T19:59:19.64981+08:00 [-] [-] [development] [I] [module.go:38] HelloWorld模块运行中...
+		2020-05-03T19:59:19.6595+08:00 [-] [-] [development] [I] [module.go:64] web: starting HTTP server :8080
+
+	通过浏览器访问
+		http://127.0.0.1:8080/say?name=mqant
+
+	访问结果
+		hi mqant
 */
