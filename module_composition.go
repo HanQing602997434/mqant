b81b8b495b7022942071aa3105e921b2d0a9d11b
hq <602997434@qq.com>
2022-02-12 02:50:16 -0800

    模块组成

--- module_composition.go
+++ module_composition.go
@@ -31,4 +31,26 @@
 	开发
 		mqant-example中的示例源码
 			房间demo源码
+
+	测试
+		func main() {
+			task := task.LoopTask{
+				C : 1, // 并发数
+			}
+			manager := table_test.NewManager(task) // 房间模型的demo
+			// manager := test_task.NewManager(task) // gate demo
+			fmt.Println("开始压测请等待")
+			task.Run(manager)
+			c := make(chan os.Signal, 1)
+			signal.Notify(c, os.Interrupt)
+			<-c
+			task.Stop()
+			os.Exit(1)
+		}
+
+		Connect...
+		OnConnectHandler
+		tabletest/HD_room_say/2 {"Trace":"0475a842d84c95ec","Error":"","Result":"success"}
+		me is 81 /room/join =》 welcome to 81
+		me is 81 /room/say =》 say hi from 81
 */
