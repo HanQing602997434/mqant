0c1a800bba416bbbe04e4e7678a7284c232465ff
hq <602997434@qq.com>
2022-02-14 18:39:44 -0800

    房间管理

--- room_manager.go
+++ room_manager.go
@@ -13,4 +13,31 @@
 			)
 			self.room = room.NewRoom(self)
 		}
+
+	创建桌子
+		CreateById(module module.RPCModule, tableId string, newTablefunc NewTableFunc) (BaseTable, error) 
+
+		//调用代码
+		table, err = self.room.CreateById(self, table_id, self.NewTable)
+		
+		CreateById
+		table_id 桌子唯一ID，作为这个房间内桌子的唯一标识
+		NewTableFunc 创建桌子的具体方法，room负责创建桌子的具体逻辑，它仅仅维护桌子在room下的对应
+
+	NewTableFunc
+		type NewTableFunc func(module module.RPCModule, tableId string) (BaseTable, error)
+
+		由开发者自己实现桌子的具体创建逻辑，如下
+			func (self *tabletest) NewTable(module module.RPCModule, tableId string) (room.BaseTable, error) {
+    			table := NewTable(
+        			module,
+        			room.TableId(tableId),
+        			room.DestroyCallbacks(func(table room.BaseTable) error {
+            			log.Info("回收了房间: %v", table.TableId())
+            			_ = self.room.DestroyTable(table.TableId())
+            			return nil
+       				}),
+    			)
+    			return table, nil
+			}
 */
