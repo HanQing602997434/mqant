74fba1c87c4e7dd37d3cda0da371c144c4201ab2
hq <602997434@qq.com>
2022-02-17 21:55:36 -0800

    桌子开发

--- table_develop.go
+++ table_develop.go
@@ -12,4 +12,14 @@
 			module module.RPCModule
 			players map[string]room.BasePlayer
 		}
+
+	第二部：必须要继承的函数
+		继承函数
+		QTable有两个结构体需要开发者提供，以实现其内部功能
+			1.GetModule()
+				返回module.RPCModule
+
+			2.GetSeats()
+				返回一个桌子内座位(用户)的map
+				GetSeats()  map[string]room.BasePlayer
 */
