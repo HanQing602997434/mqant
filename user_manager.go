be968713cea4041b1821e55412a7c9d1c08d0fe3
hq <602997434@qq.com>
2022-03-01 23:18:45 -0800

    用户管理

--- user_manager.go
+++ user_manager.go
@@ -8,4 +8,12 @@
 			通过room.QTable函数发消息跟session.Send区别在于，room.QTable函数会先将要发送
 			的消息都存放到消息队列中，待本周期执行完成以后再统一发送给客户端，发送之前会合并
 			网关，如多个用户在同一个网关且发送的消息相同只会进行一次RPC操作
+
+	给多个玩家发送消息
+		SendCallBackMsg(player []string, topic string, body []byte) error
+
+		其中players是session().GetSessionId()列表
+
+	给桌子内所有玩家广播消息
+		NotifyCallBackMsg(topic string, body []byte) error
 */
